da1f31da145fa81c7ab6e1ff0b06b5f6e7287279
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 23:50:58 +0100

    Injection de l'annotation temporal

--- src/main/java/com/jaures/cinema/dao/Seance.java
+++ src/main/java/com/jaures/cinema/dao/Seance.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,6 +21,7 @@ public class Seance {
 	@Id  @GeneratedValue(strategy=GenerationType.IDENTITY)
 	
 	private Long id;
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date heureDebut;
 	
 
