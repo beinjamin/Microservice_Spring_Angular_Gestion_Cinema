6ec586adf5d67ecdd51cd71127de6bcff8e16dec
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 20:29:31 +0100

    Mise en relation

--- src/main/java/com/jaures/cinema/dao/Projection.java
+++ src/main/java/com/jaures/cinema/dao/Projection.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 
 @Entity
@@ -15,8 +16,9 @@ public class Projection {
 	private Long id;
 	private Date dateProjection;
 	private double prix;
-	
+	@ManyToOne
 	private Salle salle;
+	@ManyToOne
 	private Film film;
 	
 
