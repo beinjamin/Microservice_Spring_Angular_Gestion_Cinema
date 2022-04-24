4b8886eda382c889f79ff689fe6f19118bd34759
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 23:33:25 +0100

    MIse en place des annotation

--- src/main/java/com/jaures/cinema/dao/Ticket.java
+++ src/main/java/com/jaures/cinema/dao/Ticket.java
@@ -1,6 +1,20 @@
 package com.jaures.cinema.dao;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ticket {
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String nomClient;
 	private double prix;
