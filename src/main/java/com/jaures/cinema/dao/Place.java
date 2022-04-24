0d304e091be70cce6530a090c8fd09fe0d4f2e1c
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 19:58:33 +0100

    IMportation des annotion jpa

--- src/main/java/com/jaures/cinema/dao/Place.java
+++ src/main/java/com/jaures/cinema/dao/Place.java
@@ -1,12 +1,15 @@
 package com.jaures.cinema.dao;
 
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Place {
 	
 	
-	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id; 
 	private int numero;
 	private double longitude,latidude,altitude;
