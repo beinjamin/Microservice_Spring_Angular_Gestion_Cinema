f58b394bbace1b999200fcc35f63449c9620de48
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 20:35:25 +0100

    Mise en place des attribut films

--- src/main/java/com/jaures/cinema/dao/Film.java
+++ src/main/java/com/jaures/cinema/dao/Film.java
@@ -1,5 +1,14 @@
 package com.jaures.cinema.dao;
 
+import java.util.Date;
+
 public class Film {
+	private Long id;
+	private String titre;
+	private String description;
+	private String realisateur;
+	private Date dateSortie;
+	private double duree;
+	private String photo;
 
 }
