0aae7261fbb52ef00937888413beae111e6380b2
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 19:15:13 +0100

    Injection de la depence lambok pour la generation des get et set

--- src/main/java/com/jaures/cinema/dao/Cinema.java
+++ src/main/java/com/jaures/cinema/dao/Cinema.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity
 @Data
 public class Cinema implements Serializable  {
