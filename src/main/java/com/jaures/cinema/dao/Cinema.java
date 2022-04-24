dc909e2b8ef545778ed4d7744f393f2dbd649ff8
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 19:14:10 +0100

    mise en place des dependence pour le mapping object relation

--- src/main/java/com/jaures/cinema/dao/Cinema.java
+++ src/main/java/com/jaures/cinema/dao/Cinema.java
@@ -2,7 +2,10 @@ package com.jaures.cinema.dao;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Cinema implements Serializable  {
