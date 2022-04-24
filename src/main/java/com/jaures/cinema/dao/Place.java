ee98a2be9790142c916a24318dfc0ed09f51dbaa
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 23:39:42 +0100

    Gestion de la relation

--- src/main/java/com/jaures/cinema/dao/Place.java
+++ src/main/java/com/jaures/cinema/dao/Place.java
@@ -1,10 +1,13 @@
 package com.jaures.cinema.dao;
 
+import java.util.Collection;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Place {
@@ -16,5 +19,7 @@ public class Place {
 	private double longitude,latidude,altitude;
 	@ManyToOne
 	private Salle salle;
+	@OneToMany(mappedBy="place")
+	private Collection<Ticket> tickets;
 
 }
