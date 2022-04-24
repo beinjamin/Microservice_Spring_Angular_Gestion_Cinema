777c241ab097a3d01694b751ce6d637957ab2079
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 20:21:01 +0100

    Declartion des attribue projetion et chaque projection contient un film

--- src/main/java/com/jaures/cinema/dao/Projection.java
+++ src/main/java/com/jaures/cinema/dao/Projection.java
@@ -1,8 +1,14 @@
 package com.jaures.cinema.dao;
 
+import java.util.Date;
+
 public class Projection {
 	private Long id;
 	private Date dateProjection;
+	private double prix;
+	
+	private 
+	private Film film;
 	
 
 }
