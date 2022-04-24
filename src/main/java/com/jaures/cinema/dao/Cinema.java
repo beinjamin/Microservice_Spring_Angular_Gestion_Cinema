87052a28aef718d93646cf625c6a4f84515c6613
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 19:17:28 +0100

    Mise en place de l'annotation tostring

--- src/main/java/com/jaures/cinema/dao/Cinema.java
+++ src/main/java/com/jaures/cinema/dao/Cinema.java
@@ -10,9 +10,10 @@ import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
-@Data @AllArgsConstructor @NoArgsConstructor
+@Data @AllArgsConstructor @NoArgsConstructor @ToString
 public class Cinema implements Serializable  {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id ;
