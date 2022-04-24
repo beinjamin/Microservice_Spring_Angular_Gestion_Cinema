6dea5fdbb334f88ada25636074945a8a4fea4d4e
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 19:49:17 +0100

    Injection des dependence lobok et jpa

--- src/main/java/com/jaures/cinema/dao/Ville.java
+++ src/main/java/com/jaures/cinema/dao/Ville.java
@@ -3,20 +3,24 @@ package com.jaures.cinema.dao;
 import java.util.Collection;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 
 
 
 
 @Entity
-@Data
-@NoArgsContructor
-@ArgsConstructor
+
+
 public class Ville {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long Id;
+	private Long id;
 	private String name;
 	private double logitude,latidude,altitude;
+	@OneToMany
 	private Collection<Cinema> cinemas;
 
 }
