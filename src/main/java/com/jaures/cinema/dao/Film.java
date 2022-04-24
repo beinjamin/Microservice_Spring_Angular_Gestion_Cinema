63eaca7bde3127997ea5fe7d97cc3e4e11797cca
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 20:46:32 +0100

    MIse en relaionfilm

--- src/main/java/com/jaures/cinema/dao/Film.java
+++ src/main/java/com/jaures/cinema/dao/Film.java
@@ -3,6 +3,18 @@ package com.jaures.cinema.dao;
 import java.util.Collection;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data 
+@NoArgsConstructor
+@AllArgsConstructor
 public class Film {
 	private Long id;
 	private String titre;
@@ -11,6 +23,8 @@ public class Film {
 	private Date dateSortie;
 	private double duree;
 	private String photo;
-
+	
+    @OneToMany(mappedBy="film")
 	private Collection<Projection> projections;
+	private Categorie categorie;
 }
