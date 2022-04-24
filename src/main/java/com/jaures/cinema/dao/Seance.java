214d0d3917f37bba4890d611e7e908ba11772041
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 23:48:54 +0100

    Mise en place des attribue de seance

--- src/main/java/com/jaures/cinema/dao/Seance.java
+++ src/main/java/com/jaures/cinema/dao/Seance.java
@@ -1,5 +1,25 @@
 package com.jaures.cinema.dao;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Seance {
+	@Id  @GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+	private Long id;
+	private Date heureDebut;
+	
 
 }
