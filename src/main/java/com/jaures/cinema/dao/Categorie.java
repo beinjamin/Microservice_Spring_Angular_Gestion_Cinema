9ee68e3c103182a7dc3e75985ec76ae91c58b20d
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 23:22:25 +0100

    Injection dees annotation jpa

--- src/main/java/com/jaures/cinema/dao/Categorie.java
+++ src/main/java/com/jaures/cinema/dao/Categorie.java
@@ -2,6 +2,16 @@ package com.jaures.cinema.dao;
 
 import java.util.Collection;
 
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data 
+@NoArgsConstructor
+@AllArgsConstructor
 public class Categorie {
 	private Long id;
 	private String name;
