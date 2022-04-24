2e230da98835578e1960c177f3bab66d09bf480b
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 19:10:52 +0100

    mise en place des attribut de la class cinema

--- src/main/java/com/jaures/cinema/dao/Cinema.java
+++ src/main/java/com/jaures/cinema/dao/Cinema.java
@@ -3,5 +3,8 @@ package com.jaures.cinema.dao;
 import java.io.Serializable;
 
 public class Cinema implements Serializable  {
+	private Long id ;
+	private String name;
+	private longitude,latitude,altitude;
 
 }
