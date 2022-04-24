978f10cf3e179642bca254f20ce7decca9a2e4db
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 19:42:28 +0100

    Mise en relation entre cinema et ville

--- src/main/java/com/jaures/cinema/dao/Ville.java
+++ src/main/java/com/jaures/cinema/dao/Ville.java
@@ -1,9 +1,12 @@
 package com.jaures.cinema.dao;
 
+import java.util.Collection;
+
 public class Ville {
 	
 	private Long Id;
 	private String name;
-	private 
+	private double logitude,latidude,altitude;
+	private Collection<Cinema> cinemas;
 
 }
