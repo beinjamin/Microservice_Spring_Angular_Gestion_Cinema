8629abf364f0251dd27853c7190a76c280f84eec
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 19:33:22 +0100

    Injection des annotation jpa

--- src/main/java/com/jaures/cinema/dao/Cinema.java
+++ src/main/java/com/jaures/cinema/dao/Cinema.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- src/main/java/com/jaures/cinema/dao/Salle.java
+++ src/main/java/com/jaures/cinema/dao/Salle.java
@@ -1,7 +1,13 @@
 package com.jaures.cinema.dao;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Salle {
-	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private int nombrePlace;
