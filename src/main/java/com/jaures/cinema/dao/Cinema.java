d3438a83d4ef9aeaad3fdc09087ab8e2c45f390d
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 19:16:47 +0100

    Mise en place de l'annotion contructor pour le constructeur avec et sans parametre

--- src/main/java/com/jaures/cinema/dao/Cinema.java
+++ src/main/java/com/jaures/cinema/dao/Cinema.java
@@ -7,10 +7,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Data @AllArgsConstructor
+@Data @AllArgsConstructor @NoArgsConstructor
 public class Cinema implements Serializable  {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id ;
