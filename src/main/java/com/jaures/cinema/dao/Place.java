46c9baa71c09ac274072e801c65dcd2cef697f31
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 19:57:05 +0100

    Creation des attribue place

--- src/main/java/com/jaures/cinema/dao/Place.java
+++ src/main/java/com/jaures/cinema/dao/Place.java
@@ -1,5 +1,15 @@
 package com.jaures.cinema.dao;
 
+
+
+@Entity
 public class Place {
+	
+	
+	
+	private Long id; 
+	private int numero;
+	private double longitude,latidude,altitude;
+	private Salle salle;
 
 }
