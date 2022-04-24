44e0a07696454f219c86601299a49e9a83ae4c6a
beinjamin <jauresbeinjamin@gmail.com>
2022-04-24 19:34:22 +0100

    Injection des annotation Lombok

--- src/main/java/com/jaures/cinema/dao/Salle.java
+++ src/main/java/com/jaures/cinema/dao/Salle.java
@@ -5,7 +5,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Salle {
 	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
