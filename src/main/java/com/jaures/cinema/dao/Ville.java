0e4d3dd54b34c59666b319e226f6da0ab514f7fe
beinjamin <jauresbeinjamin@gmail.com>
2022-04-25 21:14:38 +0100

    Injection de l'annotation lambok

--- src/main/java/com/jaures/cinema/dao/Ville.java
+++ src/main/java/com/jaures/cinema/dao/Ville.java
@@ -8,12 +8,18 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 
 
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 
 public class Ville {
