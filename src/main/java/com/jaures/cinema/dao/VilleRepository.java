55857006e30600ac751dceed251bd80aa528998c
beinjamin <jauresbeinjamin@gmail.com>
2022-04-26 21:25:06 +0100

    Mise en place de api Rest via l'annotation RepositoryRestResource

--- src/main/java/com/jaures/cinema/dao/VilleRepository.java
+++ src/main/java/com/jaures/cinema/dao/VilleRepository.java
@@ -1,9 +1,11 @@
 package com.jaures.cinema.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.jaures.cinema.entities.Ville;
 
+@RepositoryRestResource
 
 public interface VilleRepository extends JpaRepository<Ville, Long>{
 
