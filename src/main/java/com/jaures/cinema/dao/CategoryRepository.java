df9ac507c05b23daf8c816c5c8e5d33448c3c8ed
beinjamin <jauresbeinjamin@gmail.com>
2022-04-26 21:17:33 +0100

    Creation des differentes interface

--- src/main/java/com/jaures/cinema/dao/CategoryRepository.java
+++
@@ -1,5 +0,0 @@
-package com.jaures.cinema.dao;
-
-public interface CategoryRepository {
-
-}
--- src/main/java/com/jaures/cinema/dao/FilmRepository.java
+++ src/main/java/com/jaures/cinema/dao/FilmRepository.java
@@ -1,5 +1,10 @@
 package com.jaures.cinema.dao;
 
-public interface FilmRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jaures.cinema.entities.Film;
+
+
+public interface FilmRepository extends JpaRepository<Film, Long>{
 
 }
