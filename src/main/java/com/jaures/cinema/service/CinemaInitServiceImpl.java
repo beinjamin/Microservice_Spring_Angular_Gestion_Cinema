a70bf4c201d27b600fb9563b0f956d9204e5f3a6
beinjamin <jauresbeinjamin@gmail.com>
2022-04-29 18:55:42 +0100

    Creation de la class qui implement cet interface

--- src/main/java/com/jaures/cinema/service/CinemaInitServiceImpl.java
+++ src/main/java/com/jaures/cinema/service/CinemaInitServiceImpl.java
@@ -1,5 +1,59 @@
 package com.jaures.cinema.service;
 
-public class CinemaInitServiceImpl {
+public class CinemaInitServiceImpl implements ICinemaInitService {
+
+	@Override
+	public void initVilles() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initCinemas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void intiSalles() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initSeances() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initPlaces() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initCategories() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initFilms() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initProjections() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initTickets() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
