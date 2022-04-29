07210ec20ad0c0cda85993cb9072cdb8305fa430
beinjamin <jauresbeinjamin@gmail.com>
2022-04-29 18:46:05 +0100

    Initialisation

--- src/main/java/com/jaures/cinema/service/ICinemaInitService.java
+++ src/main/java/com/jaures/cinema/service/ICinemaInitService.java
@@ -1,5 +1,16 @@
 package com.jaures.cinema.service;
 
 public interface ICinemaInitService {
+	
+	
+	public void initVilles();
+	public void initCinemas();
+	public void intiSalles();
+	public void initSeances();
+	public void initPlaces();
+	public void initCategories();
+	public void initFilms ();
+	public void initProjections();
+	public void initTickets();
 
 }
