802628aaf01a833aa9a61a7cc9446788bde26ab2
beinjamin <jauresbeinjamin@gmail.com>
2022-04-30 19:30:39 +0100

    update du fichier application property

--- src/main/java/com/jaures/cinema/service/ICinemaInitService.java
+++ src/main/java/com/jaures/cinema/service/ICinemaInitService.java
@@ -6,8 +6,8 @@ public interface ICinemaInitService {
 	public void initVilles();
 	public void initCinemas();
 	public void intiSalles();
-	public void initSeances();
 	public void initPlaces();
+	public void initSeances();
 	public void initCategories();
 	public void initFilms ();
 	public void initProjections();
