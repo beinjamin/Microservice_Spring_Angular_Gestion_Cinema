acbc9e064698645efafcfecf416c7078529f4d06
Beinjamin <jauresbeinjamin@gmail.com>
2022-05-17 19:24:46 +0100

    Gestion de api rest et divert autre metode

--- src/main/java/com/jaures/cinema/web/CinemaRestController.java
+++ src/main/java/com/jaures/cinema/web/CinemaRestController.java
@@ -1,21 +1,16 @@
 package com.jaures.cinema.web;
 
-import java.util.List;
-
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.jaures.cinema.dao.FilmRepository;
-import com.jaures.cinema.entities.Film;
 
 @RestController
 public class CinemaRestController {
-	private FilmRepository filmRepository;
+	@GetMapping(path="/imageFilm/{id}", produces=MediaType.IMAGE_JPEG_VALUE)
 	
-	
-	@GetMapping("/listFilms")
-	public List<Film> films(){
-		return filmRepository.findAll();
+	public byte[] image(@PathVariable (name="id")Long id) {
+		Film
 		
 	}
 	
